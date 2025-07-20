69ba55cab075bd12d680594de98b19761bfeb66b
akshay-ken <akshaybest3120@gmail.com>
2025-07-20 11:21:34 +0530

    add: added state and desktop layout styles

--- src/components/TopSection.jsx
+++ src/components/TopSection.jsx
@@ -1,15 +1,24 @@
 import { MdNavigateNext } from "react-icons/md";
 import { MdNavigateBefore } from "react-icons/md";
-export function TopSection() {
+import AvatarTanya from "../assets/images/image-tanya.jpg";
+import clsx from "clsx";
+export function TopSection({ image, body, title, name, onClick }) {
+  const baseClass = clsx(
+    "size-64 md:size-96 rounded-lg bg-cover bg-center bg-no-repeat shadow-black shadow-2xl",
+    {
+      "bg-[url('./assets/images/image-tanya.jpg')]": image == "F",
+      "bg-[url('./assets/images/image-john.jpg')]": image == "M",
+    }
+  );
   return (
-    <section className="w-full h-1/2">
+    <section className="w-full h-1/2 md:h-screen">
       <div className="bg-[url('./assets/images/pattern-bg.svg')] bg-contain h-full bg-no-repeat bg-center flex items-center justify-center">
-        <div className="bg-[url('./assets/images/image-tanya.jpg')] size-64 rounded-lg bg-cover bg-center bg-no-repeat shadow-black shadow-2xl">
-          <div className="bg-white h-9 w-fit rounded-full my-60 ml-6">
-            <button>
+        <div className={baseClass}>
+          <div className="bg-white h-9 w-fit rounded-full my-60 md:my-92 ml-6">
+            <button onClick={onClick}>
               <MdNavigateBefore className="size-10" />
             </button>
-            <button>
+            <button onClick={onClick}>
               <MdNavigateNext className="size-10" />
             </button>
           </div>
